d0ca88d08f48296f331f3e3a78b86b0f15d8da2e
richtwin567 <anakai.richards@gmail.com>
2021-03-04 19:31:16 -0500

    step 8 complete

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
 
+from werkzeug.security import check_password_hash
 from app import app, db, login_manager
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
@@ -34,20 +35,30 @@ def login():
     if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        if form.validate_on_submit():
             # Get the username and password values from the form.
-
+            username= form.username
+            password = form.password
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
             # You will need to import the appropriate function to do so.
             # Then store the result of that query to a `user` variable so it can be
             # passed to the login_user() method below.
-
+            should_login = False
+            user = UserProfile.query.filter_by(username=username).first()
+            if user:
+                should_login = check_password_hash(user.password, password)
+            else:
+                flash(f"The user {username} does not exist", "error")
             # get user id, load into session
-            login_user(user)
+            if should_login:
+                login_user(user)
+                flash("Login in successful", "success")
+                return redirect(url_for("secure_page"))  # they should be redirected to a secure-page route instead
+            else:
+                flash("Incorrect password for user", "error")
 
             # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
 
